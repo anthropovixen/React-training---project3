b671d25563206be6fe0216b383fbfa83d4df1370
Tanimara Santos <36615712+anthropovixen@users.noreply.github.com>
2022-02-04 11:52:56 +1100

    Create Navbar.js

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import airbnblogo from "../images/airbnb-logo.png";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <img src={airbnblogo} alt="airbnb logo" className="nav--logo" />
+    </nav>
+  );
+}
