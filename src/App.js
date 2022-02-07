0df7f93b4feebbac1714b7787c8a1b7081324aba
Tanimara Santos <36615712+anthropovixen@users.noreply.github.com>
2022-02-07 15:17:40 +1100

    Update App.js
    Remove detailed props from App to insert them as an item from Card

--- src/App.js
+++ src/App.js
@@ -7,18 +7,7 @@ import data from "./data.js";
 
 export default function App() {
   const cards = data.map((item) => {
-    return (
-      <Card
-        key={item.id}
-        img={item.coverImg}
-        rating={item.stats.rating}
-        reviewCount={item.stats.reviewCount}
-        country={item.location}
-        title={item.title}
-        price={item.price}
-        openSpots={item.openSpots}
-      />
-    );
+    return <Card key={item.id} item={item} />;
   });
   return (
     <div>
