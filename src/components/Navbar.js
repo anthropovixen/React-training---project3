3636babc26700b98b53b4debc9f674a87f7c4072
Tanimara Santos <36615712+anthropovixen@users.noreply.github.com>
2022-02-08 09:19:33 +1100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <nav>
       <img
-        src="../airbnb-logo.png"
+        src="../images/airbnb-logo.png"
         alt="airbnb logo"
         className="nav--logo"
       />
