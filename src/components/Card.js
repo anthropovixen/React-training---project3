abe9c98a9fcc46a867bd5b6883b4dd38c902f41c
Tanimara Santos <36615712+anthropovixen@users.noreply.github.com>
2022-02-07 15:18:10 +1100

    Update Card.js
    Change props fetching to fetch information from item coming from data.js file

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,30 +1,32 @@
 import React from "react";
 
 export default function Card(props) {
-  let badgeText
-  if (props.openSpots === 0) {
-    badgeText = "SOLD OUT"
-  } else if (props.country === "Online") {
-    badgeText = "ONLINE"
+  let badgeText;
+  if (props.item.openSpots === 0) {
+    badgeText = "SOLD OUT";
+  } else if (props.item.location === "Online") {
+    badgeText = "ONLINE";
   }
 
   return (
     <div className="card">
       {badgeText && <div className="card--badge">{badgeText}</div>}
       <img
-        src={props.img}
+        src={props.item.coverImg}
         className="card--image"
         alt="representation of experience"
       />
       <div className="card--stats">
         <img src="../images/star.png" className="card--star" alt="red star" />
-        <span>{props.rating}</span>
-        <span className="card--gray"> ({props.reviewCount}) • </span>
-        <span className="card--gray">{props.country}</span>
-        <p>{props.title}</p>
+        <span>{props.item.stats.rating}</span>
+        <span className="card--gray"> ({props.item.stats.reviewCount}) • </span>
+        <span className="card--gray">{props.item.country}</span>
+        <p>{props.item.title}</p>
         <p>
-          <span className="card--bold card--price">From ${props.price}</span> /
-          person
+          <span className="card--bold card--price">
+            From ${props.item.price}
+          </span>{" "}
+          / person
         </p>
       </div>
     </div>
