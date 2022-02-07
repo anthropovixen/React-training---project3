9f48546674ba9182df99941ef5aadb2cbcd1be1b
Tanimara Santos <36615712+anthropovixen@users.noreply.github.com>
2022-02-07 12:18:26 +1100

    Update Card.js
    Change images folder location
    Add props to card so that it can be reusable.

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,23 +1,22 @@
 import React from "react";
-import cardimage from "../images/katie-zaferes.png";
-import star from "../images/star.png";
+import star from "./images/star.png";
 
 export default function Card(props) {
   return (
     <div className="card">
       <img
-        src={cardimage}
+        src={props.img}
         className="card--image"
         alt="representation of experience"
       />
       <div className="card--stats">
         <img src={star} className="card--star" alt="red star" />
-        <span>5.0</span>
-        <span className="card--gray"> (6) • </span>
-        <span className="card--gray">USA</span>
-        <p>Life lessons with Katie Zaferes</p>
+        <span>{props.rating}</span>
+        <span className="card--gray"> ({props.reviewCount}) • </span>
+        <span className="card--gray">{props.country}</span>
+        <p>{props.title}</p>
         <p>
-          <span className="card--bold">From $136</span> / person
+          <span className="card--bold">From ${props.price}</span> / person
         </p>
       </div>
     </div>
