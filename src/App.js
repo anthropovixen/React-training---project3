43a800beebe2c1d086335d720b1c326349efeaf4
Tanimara Santos <36615712+anthropovixen@users.noreply.github.com>
2022-02-07 13:59:29 +1100

    Update App.js
    Transform card component into reusable component

--- src/App.js
+++ src/App.js
@@ -3,21 +3,26 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
-import katie from "./components/images/katie-zaferes.png";
+import data from "./data.js";
 
 export default function App() {
+  const cards = data.map((item) => {
+    return (
+      <Card
+        img={item.coverImg}
+        rating={item.stats.rating}
+        reviewCount={item.stats.reviewCount}
+        country={item.location}
+        title={item.title}
+        price={item.price}
+      />
+    );
+  });
   return (
     <div>
       <Navbar />
       <Hero />
-      <Card
-        img={katie}
-        rating="5.0"
-        reviewCount={6}
-        country="USA"
-        title="Life lessons with Katie Zaferes"
-        price={136}
-      />
+      {cards}
     </div>
   );
 }
